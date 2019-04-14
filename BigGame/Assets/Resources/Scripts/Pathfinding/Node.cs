65d7d3b8362bc4760daacd9edfaa0fe28f8fbe90
Nimbal <nimbal38@gmail.com>
2019-04-13 22:34:52 -0700

    pathfinding garbage

---
+++ BigGame/Assets/Resources/Scripts/Pathfinding/Node.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class Node {
+
+    public int gridX; //X pos in the node array
+    public int gridY; //Y pos in the node array
+
+    public bool IsWall; //Tells the program if this node is being obstructed
+    public Vector3 Position; //The world position of the node
+
+    public Node Parent; //Stores what node it previously came from so it can trace the shortest path
+
+    public int gCost; //The cost of moving to the next square
+    public int hCost; //The distance to the goal from this node
+
+    public int FCost { get { return gCost + hCost; } } //Get function to add G cost and H cost
+
+    public Node(bool _IsWall, Vector3 a_pos, int a_gridX, int a_gridY) //Constructor
+    {
+        IsWall = a_IsWall; //Tells the program if this node is being obstructed
+        Position = a_pos; //The world position of the node
+        gridX = a_gridX; //X pos in the node array
+        gridY = a_gridY; //Y pos in the node array
+    }
+}*/
