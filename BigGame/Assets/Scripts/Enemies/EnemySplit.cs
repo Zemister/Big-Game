0b2f4c9d504e7c80f381673de27afbc4af83f298
Nimbal <nimbal38@gmail.com>
2019-04-17 14:32:30 -0700

    Added new enemy movement
    Random Wander and Option for enemies to split or spawn adds on death, etc

---
+++ BigGame/Assets/Scripts/Enemies/EnemySplit.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySplit : MonoBehaviour
+{
+
+    public GameObject splitterSpawns;
+    public float minSize;
+
+    void Update()
+    {
+        if (transform.GetComponent<Enemy>().health <= 0)
+        {
+
+            if (transform.localScale.y > minSize)
+            {
+                GameObject splitterSpawn1 = Instantiate(splitterSpawns, new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z), transform.rotation) as GameObject;
+                GameObject splitterSpawn2 = Instantiate(splitterSpawns, new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z), transform.rotation) as GameObject;
+
+                splitterSpawn1.transform.localScale = new Vector3(transform.localScale.y * 0.8f, transform.localScale.y * 0.8f, transform.localScale.z);
+                splitterSpawn2.transform.localScale = new Vector3(transform.localScale.y * 0.8f, transform.localScale.y * 0.8f, transform.localScale.z);
+            }
+        }
+    }
+}
+
